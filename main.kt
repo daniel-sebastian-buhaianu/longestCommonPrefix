afcbb9f78d19b4fe32233a0062cd48129abe4e18
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-18 15:53:00 +0300

    implemented solution

--- main.kt
+++ main.kt
@@ -1,7 +1,27 @@
 fun main() {
-
+    println(longestCommonPrefix(
+        arrayOf("car", "carbon", "carsekzosp")
+    ))
 }
 
 fun longestCommonPrefix(strs: Array<String>): String {
+    // sort array of strings lexicographically in ascending order
+    strs.sort()
+
+    // find the longest common prefix of the first and last string in strs
+    // because that is the final result
+    val firstStr = strs[0]
+    val lastStr = strs[strs.lastIndex]
+    var result = ""
+    var i = 0
+    while (i < firstStr.length) {
+        if (firstStr[i] == lastStr[i]) {
+            result += firstStr[i]
+        } else {
+            break;
+        }
+        i++
+    }
     
+    return result
 }
